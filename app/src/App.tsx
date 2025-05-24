01762c7739f885352d72590d06989cc01b57be1b
Tymek <t.ilczyszyn@gmail.com>
2025-05-24 10:53:08 +0200

    fix

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,4 @@
-// src/App.tsx
-import AppRouter from './router';
-
-export default function App() {
-  return <AppRouter />;
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
